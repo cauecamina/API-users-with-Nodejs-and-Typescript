3a65976764142cc90c1944007eb3b78acad7ce8e
cauecamina <cauecamina@hotmail.com>
2025-04-13 04:30:15 -0300

    eslint added

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,12 @@ import express, { Request, Response } from 'express';
 
 const server = express();
 
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars
+interface Teste {
+    
+}
+
 server.get('/', (req: Request, res: Response) => {
   res.send('Olá Dev');
 });
